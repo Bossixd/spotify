2d465a3724a5d50f60bc7c9e210a540d54081ba0
Bossixd <bosspattakit123@gmail.com>
2024-10-01 17:12:27 -0400

    HomeLayout Hotbar

--- spotify/src/index.tsx
+++ spotify/src/index.tsx
@@ -1,8 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+// import './index.css';
 import reportWebVitals from './reportWebVitals';
+
+import HomeLayout from "./layouts/HomeLayout";
+import Home from "./pages/Home";
+import './index.css';
+// import Blogs from "./pages/Blogs";
+// import Contact from "./pages/Contact";
+// import NoPage from "./pages/NoPage";
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeLayout />}>
+          <Route index element={<Home />} />
+          {/* <Route path="blogs" element={<Blogs />} />
+          <Route path="contact" element={<Contact />} />
+          <Route path="*" element={<NoPage />} /> */}
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
---
+++ spotify/src/layouts/HomeLayout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+import Topbar from "./Topbar";
+import './HomeLayout.css';
+
+function HomeLayout() {
+    return (
+        <div className="grid-container">
+            <div className="grid-header">
+                <Topbar />
+            </div>
+            <div className="grid-menu">Menu</div>
+            <div className="grid-main">Main</div>
+            <div className="grid-right">Right</div>
+            <div className="grid-footer">Footer</div>
+        </div>
+    );
+}
+
+export default HomeLayout;
