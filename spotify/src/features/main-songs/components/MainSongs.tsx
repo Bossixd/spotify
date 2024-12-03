7ca46e8a3a9076db5bc1c3a198805dde627e07a5
Bossixd <bosspattakit123@gmail.com>
2024-12-02 23:08:45 -0500

    scalable-main-recommendation add-width-of-main

--- spotify/src/features/main-songs/components/MainSongs.tsx
+++ spotify/src/features/main-songs/components/MainSongs.tsx
@@ -1,22 +1,58 @@
-// import { useEffect, useState } from "react";
+import { useRef, useEffect, useState } from "react";
 import "./MainSongs.css";
 import MainSongsHeader from "./MainSongsHeader";
 import MainSongsItems from "./MainSongsItems";
 import MainSongsRec from "./MainSongsRec";
 
-import recommendations from "../../../data/recommendations.json"
+import recommendations from "../../../data/recommendations.json";
 
 function MainSongs() {
+    const mainRef = useRef<HTMLDivElement>(null);
+    const [width, setWidth] = useState(0);
+
+    const resizeObserver = new ResizeObserver((entries) => {
+        setWidth(entries[0].contentRect.width);
+        console.log(width);
+    });
+
+    useEffect(() => {
+        const mainElement = document.querySelector(".main-songs");
+        console.log(mainElement);
+        if (mainElement) {
+            resizeObserver.observe(mainElement);
+        }
+    }, [resizeObserver]);
+
+    useEffect(() => {
+        if (mainRef && mainRef.current) {
+            setWidth(mainRef.current.offsetWidth);
+            console.log(mainRef.current.offsetWidth);
+        }
+        console.log("Here");
+    }, []);
+
     return (
-        <div className="main-songs">
+        <div className="main-songs" ref={mainRef}>
             <MainSongsHeader /> {/* Fixed at the TOP!!!!! */}
             <div className="main-songs-main">
-                <div className="spacer"/>
+                <div className="spacer" />
                 <MainSongsItems />
-                <MainSongsRec title="Recommended for you" recommendations={recommendations}/>
-                <MainSongsRec title="Recommended for you" recommendations={recommendations}/>
-                <MainSongsRec title="Recommended for you" recommendations={recommendations}/>
-                <MainSongsRec title="Recommended for you" recommendations={recommendations}/>
+                <MainSongsRec
+                    title="Recommended for you"
+                    recommendations={recommendations}
+                />
+                <MainSongsRec
+                    title="Recommended for you"
+                    recommendations={recommendations}
+                />
+                <MainSongsRec
+                    title="Recommended for you"
+                    recommendations={recommendations}
+                />
+                <MainSongsRec
+                    title="Recommended for you"
+                    recommendations={recommendations}
+                />
             </div>
         </div>
     );
