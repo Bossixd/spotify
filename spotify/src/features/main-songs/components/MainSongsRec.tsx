a06a34c49720565e9b82099b705201d4bce5b6f4
Bossixd <bosspattakit123@gmail.com>
2024-12-02 22:33:03 -0500

    merge hide-sidebars

--- spotify/src/features/main-songs/components/MainSongsRec.tsx
+++ spotify/src/features/main-songs/components/MainSongsRec.tsx
@@ -1,6 +1,7 @@
 import "./MainSongs.css";
 
-import image from "../../../assets/alan-walker.jpg"
+import image from "../../../assets/alan-walker.jpg";
+import { useEffect } from "react";
 
 interface Props {
     title: string;
@@ -11,14 +12,20 @@ interface Props {
 }
 
 function MainSongsRec({ title, recommendations }: Props) {
+    useEffect(() => {
+        // Set the value of the CSS variable
+        document.documentElement.style.setProperty("--image-width", "250px");
+    }, []);
     return (
         <div className="main-songs-rec-section">
             <div className="main-songs-rec-title">{title}</div>
             <div className="main-songs-rec-items">
                 {recommendations.map((item) => (
                     <div className="main-songs-rec-item">
-                        <img className="main-songs-rec-item-image" src={image}>
-                        </img>
+                        <img
+                            className="main-songs-rec-item-image"
+                            src={image}
+                        ></img>
                         <span className="main-songs-rec-item-detail">
                             {item.description}
                         </span>
